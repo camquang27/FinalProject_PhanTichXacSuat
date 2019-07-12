1a4eef8dff34a7620a204b658073d961de2ff5e4
camquang27 <camquang27@gmail.com>
2019-07-13 03:09:45 +0700

    Add files via upload

---
+++ Demo/features_extraction.py
@@ -0,0 +1,96 @@
+import logging
+
+import pandas as pd
+import numpy as np
+
+log = logging.getLogger(__name__)
+
+
+def rate_of_change(df, n):
+    """
+    
+    :param df: pandas.DataFrame
+    :param n: 
+    :return: pandas.DataFrame
+    """
+    M = df['Close'].diff(n - 1)
+    N = df['Close'].shift(n - 1)
+    ROC = pd.Series(M / N, name='ROC_' + str(n))
+    df = df.join(ROC)
+    return df
+
+
+
+def macd(df, n_fast, n_slow):
+    """Calculate MACD, MACD Signal and MACD difference
+    
+    :param df: pandas.DataFrame
+    :param n_fast: 
+    :param n_slow: 
+    :return: pandas.DataFrame
+    """
+    EMAfast = pd.Series(df['Close'].ewm(span=n_fast, min_periods=n_slow).mean())
+    EMAslow = pd.Series(df['Close'].ewm(span=n_slow, min_periods=n_slow).mean())
+    MACD = pd.Series(EMAfast - EMAslow, name='MACD_' + str(n_fast) + '_' + str(n_slow))
+    MACDsign = pd.Series(MACD.ewm(span=9, min_periods=9).mean(), name='MACDsign_' + str(n_fast) + '_' + str(n_slow))
+    MACDdiff = pd.Series(MACD - MACDsign, name='MACDdiff_' + str(n_fast) + '_' + str(n_slow))
+    df = df.join(MACD)
+    df = df.join(MACDsign)
+    df = df.join(MACDdiff)
+    return df
+
+
+
+def relative_strength_index(df, n):
+    """Calculate Relative Strength Index(RSI) for given data.
+    
+    :param df: pandas.DataFrame
+    :param n: 
+    :return: pandas.DataFrame
+    """
+    i = 0
+    UpI = [0]
+    DoI = [0]
+    while i + 1 <= df.index[-1]:
+        UpMove = df.loc[i + 1, 'High'] - df.loc[i, 'High']
+        DoMove = df.loc[i, 'Low'] - df.loc[i + 1, 'Low']
+        if UpMove > DoMove and UpMove > 0:
+            UpD = UpMove
+        else:
+            UpD = 0
+        UpI.append(UpD)
+        if DoMove > UpMove and DoMove > 0:
+            DoD = DoMove
+        else:
+            DoD = 0
+        DoI.append(DoD)
+        i = i + 1
+    UpI = pd.Series(UpI)
+    DoI = pd.Series(DoI)
+    PosDI = pd.Series(UpI.ewm(span=n, min_periods=n).mean())
+    NegDI = pd.Series(DoI.ewm(span=n, min_periods=n).mean())
+    RSI = pd.Series(PosDI / (PosDI + NegDI), name='RSI_' + str(n))
+    df = df.join(RSI)
+    return df
+
+def on_balance_volume(df, n):
+    """Calculate On-Balance Volume for given data.
+    
+    :param df: pandas.DataFrame
+    :param n: 
+    :return: pandas.DataFrame
+    """
+    i = 0
+    OBV = [0]
+    while i < df.index[-1]:
+        if df.loc[i + 1, 'Close'] - df.loc[i, 'Close'] > 0:
+            OBV.append(df.loc[i + 1, 'Volume'])
+        if df.loc[i + 1, 'Close'] - df.loc[i, 'Close'] == 0:
+            OBV.append(0)
+        if df.loc[i + 1, 'Close'] - df.loc[i, 'Close'] < 0:
+            OBV.append(-df.loc[i + 1, 'Volume'])
+        i = i + 1
+    OBV = pd.Series(OBV)
+    OBV_ma = pd.Series(OBV.rolling(n, min_periods=n).mean(), name='OBV_' + str(n))
+    df = df.join(OBV_ma)
+    return df
